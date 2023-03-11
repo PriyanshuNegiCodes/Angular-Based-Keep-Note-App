e6b625d34f4ef12c4882abfb4df962cb3e85d764
Priyanshu Negi <priyanshunegi2398@gmail.com>
2023-03-11 15:02:38 +0530

    udpates

--- angular/cli/src/app/note/note.component.ts
+++ angular/cli/src/app/note/note.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Note } from 'src/assets/note';
+import { NotesService } from '../services/notes.service';
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent {
-
+  NoteData: Note|any;
 }
