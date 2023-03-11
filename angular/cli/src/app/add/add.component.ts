bbefcb93e4328ebb256ed2e62811672d1cfe41c8
Priyanshu Negi <priyanshunegi2398@gmail.com>
2023-03-11 12:14:47 +0530

    done

--- angular/cli/src/app/add/add.component.ts
+++ angular/cli/src/app/add/add.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Note } from 'src/assets/note';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
-
+  emptyNote: Note = {
+    // id: null,
+    title: "",
+    content: "",
+    reminderDate: "",
+    category: "",
+    priority: ""
+  };
 }
