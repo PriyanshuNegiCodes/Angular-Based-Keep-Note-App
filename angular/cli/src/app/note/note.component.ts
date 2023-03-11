11353e1238d73f0cdaf8df676fd63f592544437e
Priyanshu Negi <priyanshunegi2398@gmail.com>
2023-03-11 20:41:21 +0530

    changes

--- angular/cli/src/app/note/note.component.ts
+++ angular/cli/src/app/note/note.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Note } from 'src/assets/note';
 import { NotesService } from '../services/notes.service';
+
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
@@ -9,10 +10,22 @@ import { NotesService } from '../services/notes.service';
 export class NoteComponent {
   NoteData: Note|any;
 
-  constructor(private myservice:NotesService){
-  }
+  constructor(private myservice:NotesService){}
 
   ngOnInit(){
-   this.myservice.getNotes().subscribe(data=>this.NoteData=data)
+    this.myservice.getNotes().subscribe(data => this.NoteData = data);
+  }
+
+  dateChange(input: any) {
+    let date = new Date(input);
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    };
+    return date.toLocaleString('en-US', options);
   }
 }
